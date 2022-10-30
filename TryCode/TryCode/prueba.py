9fa01a1057a63607a98e02af88a314b1de6e1f7c
Ronald Blanco <ronald.blanco.navarro@est.una.ac.cr>
2022-10-29 23:25:43 -0600

    aaaaa

---
+++ TryCode/TryCode/prueba.py
@@ -0,0 +1,14 @@
+def hola():
+    print("hola")
+
+class Prueba1:
+    def __init__(self):
+        self.p = None
+        self.tokens={ "TT_INT"	: 'INT', "TT_FLOAT"    : 'FLOAT', "TT_PLUS"     : 'PLUS', "TT_MINUS"    : 'MINUS', "TT_MUL"      : 'MUL', "TT_DIV"      : 'DIV', "TT_LPAREN"   : 'LPAREN', "TT_RPAREN"   : 'RPAREN'}    
+    
+    def comp(self, hola):
+        self.p = hola
+        print(self.p)
+    
+    def mostrarToken(self, tk):
+        print(self.tokens[tk])
